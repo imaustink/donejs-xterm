c2dab1762ccdd1af523fbe68c791838b11b3f5f5
imaustink <austin@austinkurpuis.com>
2017-05-04 17:19:09 -0600

    Got input working :)

--- src/donejs-xterm.js
+++ src/donejs-xterm.js
@@ -4,6 +4,8 @@ import view from './donejs-xterm.stache';
 import Terminal from 'xterm';
 import 'xterm/dist/xterm.css';
 
+const prompt = 'Cake is a lie $ ';
+
 export const Xterm = DefineMap.extend({
   api: {
     type: 'object',
@@ -19,7 +21,30 @@ export default Component.extend({
 		inserted(el) {
       if(System.isPlatform("window")) {
         var vm = this.viewModel;
-        vm.api.open(el.childNodes[0]);
+        var api = vm.api;
+        api.open(el.childNodes[0]);
+        
+        api.prompt = function () {
+          api.write('\r\n' + prompt);
+        };
+        api.write(prompt);
+
+        api.on('key', function (key, ev) {
+          var printable = (
+            !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
+          );
+
+          if (ev.keyCode === 13) {
+            api.prompt();
+          } else if (ev.keyCode === 8) {
+            // Do not delete the prompt
+            if (api.x > prompt.length) {
+              api.write('\b \b');
+            }
+          } else if (printable) {
+            api.write(key);
+          }
+        });
       }
 		}
 	}
